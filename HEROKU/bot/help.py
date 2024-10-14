afaa7d43632b03d5eb37b91e0052493caec01493
SEX-SUX <sachinxpapa@gmail.com>
2024-10-14 13:29:47 +0530

    Ready For Deploy Any Bot !

--- HEROKU/bot/help.py
+++ HEROKU/bot/help.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from HEROKU import app
 
 
-@app.on_message(filters.command(["start"]))
+@app.on_message(filters.command(["help"]))
 async def start(client, message):
     await message.reply_text(
         f"Hello there! I am heroku control bot\n\nCheck hosted apps: /myhost, /heroku."
